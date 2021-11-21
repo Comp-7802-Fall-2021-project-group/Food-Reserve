1961189c1936faf0ccfd61a6b187c0b380ed09c7
Phil <philfeiteng@gmail.com>
2021-11-20 22:39:21 -0800

    add recipe database

--- app/src/main/java/com/example/foodreserve/database/RecipeDB.java
+++ app/src/main/java/com/example/foodreserve/database/RecipeDB.java
@@ -1,2 +1,26 @@
-package com.example.foodreserve.database;public class RecipeDB {
+package com.example.foodreserve.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.foodreserve.model.Recipe;
+
+@Database(entities = {Recipe.class}, version = 1)
+public abstract class RecipeDB extends RoomDatabase {
+
+    public abstract RecipeDao recipeDao();
+
+    private static final String DB_NAME = "recipe_db";
+
+    private static RecipeDB instance;
+
+    public static synchronized RecipeDB getInstance(Context context) {
+        if(instance != null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), RecipeDB.class, DB_NAME).fallbackToDestructiveMigration().build();
+        }
+        return instance;
+    }
 }
