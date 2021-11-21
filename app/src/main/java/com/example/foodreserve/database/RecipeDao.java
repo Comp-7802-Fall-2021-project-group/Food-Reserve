2e9ed32a8bb487733f36ba505b01d48fb5222990
Phil <philfeiteng@gmail.com>
2021-11-20 22:39:53 -0800

    add recipe data access obj

--- app/src/main/java/com/example/foodreserve/database/RecipeDao.java
+++ app/src/main/java/com/example/foodreserve/database/RecipeDao.java
@@ -1,2 +1,33 @@
-package com.example.foodreserve.database;public interface RecipeDao {
+package com.example.foodreserve.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.foodreserve.model.Recipe;
+
+import java.util.List;
+
+@Dao
+public interface RecipeDao {
+
+    @Query("SELECT * FROM recipes")
+    List<Recipe> getAllRecipes();
+
+    @Query("SELECT * FROM recipes WHERE rid = :rid")
+    Recipe getRecipeById(int rid);
+
+    @Query("SELECT * FROM recipes WHERE label = :label")
+    Recipe getRecipeByLabel(String label);
+
+    @Insert
+    void insertRecipe(Recipe recipe);
+
+    @Update
+    void updateRecipe(Recipe newRecipe);
+
+    @Delete
+    void deleteRecipe(Recipe recipe);
 }
