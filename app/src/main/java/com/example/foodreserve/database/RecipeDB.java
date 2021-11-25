d938fdf7661d5632f22bd3348e387aa0bb6c1621
Phil <philfeiteng@gmail.com>
2021-11-24 21:52:35 -0800

    commented out singleton

--- app/src/main/java/com/example/foodreserve/database/RecipeDB.java
+++ app/src/main/java/com/example/foodreserve/database/RecipeDB.java
@@ -16,14 +16,15 @@ public abstract class RecipeDB extends RoomDatabase {
 
     public abstract RecipeDao recipeDao();
 
-    private static final String DB_NAME = "recipe_db";
-
-    private static RecipeDB instance;
-
-    public static synchronized RecipeDB getInstance(Context context) {
-        if(instance != null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(), RecipeDB.class, DB_NAME).fallbackToDestructiveMigration().build();
-        }
-        return instance;
-    }
+/** Singleton Pattern */
+//    private static final String DB_NAME = "recipe_db";
+//
+//    private static RecipeDB instance;
+//
+//    public static synchronized RecipeDB getInstance(Context context) {
+//        if(instance != null) {
+//            instance = Room.databaseBuilder(context.getApplicationContext(), RecipeDB.class, DB_NAME).fallbackToDestructiveMigration().build();
+//        }
+//        return instance;
+//    }
 }
