603c9bd5ec7f9cb78706052a12ae6eff0cfcce42
Kae Ramirez <19963267+iamkaith@users.noreply.github.com>
2021-11-28 23:15:05 -0800

    Change layout and Espresso tests

--- app/src/androidTest/java/com/example/foodreserve/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/foodreserve/ExampleInstrumentedTest.java
@@ -1,11 +1,13 @@
 package com.example.foodreserve;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -37,6 +39,12 @@ import foodreserve.R;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+
+/*
+ * As object detection is run immediately on UIThread we had to introduce Thread.sleep in order to run these tests
+ * Otherwise the espresso clicking tests go very quickly
+ */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
@@ -44,39 +52,40 @@ public class ExampleInstrumentedTest {
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void test11_useAppContext() {
+    public void test11_useAppContext() throws InterruptedException {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.foodreserve", appContext.getPackageName());
+        Thread.sleep(2000);
     }
 
     // Move to next picture (move right) and go back to previous picture (left)
     @Test
-    public void test21_galleryNavigation() {
-        navigateRightLeft();
+    public void test21_galleryNavigation() throws InterruptedException {
+        onView(withId(R.id.buttonRight)).perform(click());
+        Thread.sleep(5000);
+        onView(withId(R.id.buttonLeft)).perform(click());
+        Thread.sleep(5000);
     }
 
     // Move to next picture (move right) and go back to previous picture (left)
     @Test
-    public void test22_galleryUpdateCaptionNavigation() {
-        // set caption for the last picture
-        navigateAllRight();
+    public void test22_galleryUpdateCaptionNavigation() throws InterruptedException {
+        onView(withId(R.id.buttonRight)).perform(click());
+        Thread.sleep(5000);
         // enter text string
         onView(withId(R.id.editTextCaption)).perform(replaceText(""), closeSoftKeyboard());
         onView(withId(R.id.editTextCaption)).perform(typeText("test caption from espresso"), closeSoftKeyboard());
         // press update and check match
         onView(withId(R.id.btnUpdateCaption)).perform(click());
         onView(withId(R.id.editTextCaption)).check(matches(withText("test caption from espresso")));
-
-        // move away 2 pics, return and check match
-        navigateLeftRight();
-        onView(withId(R.id.editTextCaption)).check(matches(withText("test caption from espresso")));
     }
 
     // Move to next picture (move right) and go back to previous picture (left)
     @Test
-    public void test23_galleryUpdateCaptionNavigation() {
-        navigateAllRight();
+    public void test23_galleryUpdateCaptionNavigation() throws InterruptedException {
+        onView(withId(R.id.buttonRight)).perform(click());
+        Thread.sleep(5000);
         onView(withId(R.id.editTextCaption)).check(matches(withText("test caption from espresso")));
 
         // enter new text string
@@ -87,188 +96,25 @@ public class ExampleInstrumentedTest {
         onView(withId(R.id.editTextCaption)).check(matches(withText("another caption from espresso")));
     }
 
-    @Test
-    public void test24_galleryUpdateCaptionNavigation() {
-        navigateAllRight();
-        onView(withId(R.id.editTextCaption)).check(matches(withText("another caption from espresso")));
-    }
-
-    // Find a photo by a specific date range & keyword
-    @Test
-    public void test31_searchByDateCancel() {
-        // pick start day
-//        onView(withId(R.id.buttonSearch)).perform(click());
-        onView(withId(R.id.startDate)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021, 10, 1));
-        onView(withText("OK")).perform(click());
-        onView(withId(R.id.startDate)).check(matches(withText("20211001")));
-
-        // pick end day
-        onView(withId(R.id.endDate)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021, 10, 5));
-        onView(withText("OK")).perform(click());
-        onView(withId(R.id.endDate)).check(matches(withText("20211005")));
-
-        // click cancel
-        onView(withId(R.id.buttonCancel)).perform(click());
-    }
-
-    // Find a photo by a specific date range & keyword
-    @Test
-    public void test32_searchByOutOfRangeDateSubmit() {
-        // pick start day
-//        onView(withId(R.id.buttonSearch)).perform(click());
-        onView(withId(R.id.startDate)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021, 10, 1));
-        onView(withText("OK")).perform(click());
-        onView(withId(R.id.startDate)).check(matches(withText("20211001")));
-
-        // pick end day
-        onView(withId(R.id.endDate)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021, 10, 2));
-        onView(withText("OK")).perform(click());
-        onView(withId(R.id.endDate)).check(matches(withText("20211002")));
-
-        // click cancel
-        onView(withId(R.id.buttonSubmit)).perform(click());
-
-        // try to go left and right
-        navigateRightLeft();
-
-    }
-
-    // Find a photo by a specific date range & keyword
-    @Test
-    public void test33_searchByInRangeDateSubmit() {
-        // pick start day
-//        onView(withId(R.id.buttonSearch)).perform(click());
-        onView(withId(R.id.startDate)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021, 10, 11));
-        onView(withText("OK")).perform(click());
-        onView(withId(R.id.startDate)).check(matches(withText("20211011")));
-
-        // pick end day
-        onView(withId(R.id.endDate)).perform(click());
-        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2021, 10, 12));
-        onView(withText("OK")).perform(click());
-        onView(withId(R.id.endDate)).check(matches(withText("20211012")));
-
-        // click submit
-        onView(withId(R.id.buttonSubmit)).perform(click());
-
-        // try to go left and right
-        navigateRightLeft();
-
-    }
-
-    // Find a photo by a specific date range & keyword
-    @Test
-    public void test34_searchByNonExistKeyword() {
-        searchByKeyword("nonsense");
+   @Test
+    public void test35_detect() throws InterruptedException {
+        onView(withId(R.id.buttonDetect)).perform(click());
+       Thread.sleep(5000);
+       onView(withId(R.id.editTextDetectedObject)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void test35_searchByExistKeyword() {
-        searchByKeyword("test1");
-        searchByKeyword("test2");
-        searchByKeyword("test3");
-        searchByKeyword("test4");
-        searchByKeyword("test5");
-        searchByKeyword("test1 test3");
-        searchByKeyword("test2 test4");
-        searchByKeyword("test1 test5");
-        searchByKeyword("espresso");
+    public void test38_viewRecipes() throws InterruptedException {
+        onView(withId(R.id.buttonDetect)).perform(click());
+        Thread.sleep(5000);
+        onView(withId(R.id.buttonRecipes)).perform(click());
+        Thread.sleep(5000);
+        pressBack();
     }
-
-    @Test
-    public void test36_searchByEmptyLatLong() {
-        searchByLatLong(null, null);
-        searchByLatLong("0", null);
-        searchByLatLong(null, "0");
-    }
-
-    @Test
-    public void test36_searchByValidLatLong() {
-        searchByLatLong("0", "0");
-        searchByLatLong("49.2414", "-123.0584");
-        searchByLatLong("49.4951", "-123.1767");
-        searchByLatLong("37.4219", "-122.0841");
-        searchByLatLong("48.9631", "-122.3763");
-    }
-
-
-    // Navigation methods
-    public void navigateAllLeft() {
-        int count = MainActivity.getPhotoCount();
-        for (int i = 0; i < count; i++) {
-            onView(withId(R.id.buttonLeft)).perform(click());
-        }
-    }
-
-    public void navigateAllRight() {
-        int count = MainActivity.getPhotoCount();
-        for (int i = 0; i < count; i++) {
-            onView(withId(R.id.buttonRight)).perform(click());
-        }
-    }
-
-    public void navigateRightLeft() {
-        // try to go left and right
-        navigateAllRight();
-        navigateAllLeft();
-    }
-
-    public void navigateLeftRight() {
-        // try to go left and right
-        navigateAllLeft();
-        navigateAllRight();
-    }
-
-
-    // Find a photo by a specific date range & keyword
-    public void searchByKeyword(String keyword) {
-        // pick start day
-//        onView(withId(R.id.buttonSearch)).perform(click());
-
-        //enter keyword
-        onView(withId(R.id.editKeywordSearch)).perform(replaceText(""), closeSoftKeyboard());
-        onView(withId(R.id.editKeywordSearch)).perform(typeText(keyword), closeSoftKeyboard());
-
-        // click submit
-        onView(withId(R.id.buttonSubmit)).perform(click());
-
-        // navigate right left
-        navigateRightLeft();
-    }
-
-    // Find a photo by a specific date range & keyword
-    public void searchByLatLong(String latitude, String longitude) {
-        // pick start day
-//        onView(withId(R.id.buttonSearch)).perform(click());
-
-        //enter keyword
-        if (latitude != null) {
-            onView(withId(R.id.latitude)).perform(replaceText(""), closeSoftKeyboard());
-            onView(withId(R.id.latitude)).perform(typeText(latitude), closeSoftKeyboard());
-        }
-
-        if (longitude != null) {
-            onView(withId(R.id.longitude)).perform(replaceText(""), closeSoftKeyboard());
-            onView(withId(R.id.longitude)).perform(typeText(longitude), closeSoftKeyboard());
-        }
-
-        // click submit
-        onView(withId(R.id.buttonSubmit)).perform(click());
-
-        // navigate right left
-        navigateRightLeft();
-    }
-
 
     public void restartActivity() {
         ActivityScenario<MainActivity> scenario = activityScenarioRule.getScenario();
         scenario.moveToState(Lifecycle.State.RESUMED);
         scenario.recreate();
     }
-
 }
