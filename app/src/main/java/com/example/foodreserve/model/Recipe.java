67bd580978b14f3bf54ff42eb043aeeb9e85a76a
Kae Ramirez <19963267+iamkaith@users.noreply.github.com>
2021-11-28 20:01:43 -0800

    Remove unused imports

--- app/src/main/java/com/example/foodreserve/model/Recipe.java
+++ app/src/main/java/com/example/foodreserve/model/Recipe.java
@@ -2,7 +2,6 @@ package com.example.foodreserve.model;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import java.util.ArrayList;
